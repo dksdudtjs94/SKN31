aa06ff9d40b05643ef80746fae0aa2c32ac868e8
An Youngsun <dksdud94@naver.com>
2026-04-15 17:53:09 +0900

    20260415 수업

---
+++ run.py
@@ -0,0 +1,9 @@
+# calc 모듈의 함수들을 호출하려면 calc 모듈을 import 해야한다.
+# calc.py -> 모듈이름: 파일명까지.
+import calc
+
+result = calc.plus(20, 30) # 모듈이름.사용할 함수() 호출
+print(result)
+print(calc.minus(100, 20))
+
+# python run.py
