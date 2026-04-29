fbcc8d7be7006d050089f01a019406478017cabb
An Youngsun <dksdud94@naver.com>
2026-04-29 17:49:34 +0900

    20260429 수업내용

--- TestProject/pages/DB_Query.sql
+++
@@ -1,8 +0,0 @@
--- faq table
-CREATE TABLE IF NOT EXISTS car_faq (
-    faq_id INT AUTO_INCREMENT PRIMARY KEY,
-    category VARCHAR(50) NOT NULL,
-    question TEXT NOT NULL,
-    answer TEXT NOT NULL,
-    site VARCHAR(100)
-);
